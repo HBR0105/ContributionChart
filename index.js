27ca4ba20ef2a004e3e391c0b58cd4ea9c85fd94
HimashRajapaksha <rajapakshahimash@gmail.com>
2024-08-28 13:54:38 +0530

    Commit on 2024-08-28T13:54:38+05:30

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+// const FILE_PATH = "./data.json";
+// const simpleGit = require("simple-git");
+// const jsonfile = require("jsonfile");
+// const moment = require("moment");
+// const random = require("random");
+
+// // Change the working directory to where your local repository is located
+// const git = simpleGit('E:/Git graph/Git bot/ContributionChart');
+
+// const makeCommit = (n) => {
+//   if (n === 0) {
+//     // Push changes to the remote repository at the end
+//     git.push(["-u", "origin", "main"], (err, result) => {
+//       if (err) {
+//         console.error("Error pushing to remote:", err);
+//       } else {
+//         console.log("Pushed all changes to the remote repository");
+//       }
+//     });
+//     return;
+//   }
+
+//   const x = random.int(0, 54);
+//   const y = random.int(0, 6);
+//   const DATE = moment()
+//     .subtract(1, "y")  // Subtract one year from the current date
+//     .add(1, "d")       // Add one day
+//     .add(x, "w")       // Add random weeks
+//     .add(y, "d")       // Add random days
+//     .format();
+
+//   const data = {
+//     date: DATE,
+//   };
+//   console.log(`Committing with date: ${DATE}`);
+
+//   jsonfile.writeFile(FILE_PATH, data, () => {
+//     git
+//       .add([FILE_PATH])
+//       .commit(DATE, { "--date": DATE })
+//       .then(() => {
+//         console.log(`Committed: ${DATE}`);
+//         makeCommit(--n);
+//       })
+//       .catch((err) => {
+//         console.error("Error during commit:", err);
+//       });
+//   });
+// };
+
+// makeCommit(120);
 const FILE_PATH = "./data.json";
 const simpleGit = require("simple-git");
 const jsonfile = require("jsonfile");
@@ -5,48 +56,43 @@ const moment = require("moment");
 const random = require("random");
 
 // Change the working directory to where your local repository is located
-const git = simpleGit("/Users/rohitaggarwal/Desktop/test/GitHub_Graph");
+const git = simpleGit('E:/Git graph/Git bot/ContributionChart');
 
 const makeCommit = (n) => {
   if (n === 0) {
-    // Push changes to the remote repository
-    git.push(["-u", "origin", "master"], (err, result) => {
+    // Push changes to the remote repository at the end
+    git.push(["-u", "origin", "main"], (err, result) => {
       if (err) {
         console.error("Error pushing to remote:", err);
       } else {
-        console.log("Pushed changes to remote repository");
+        console.log("Pushed all changes to the remote repository");
       }
     });
     return;
   }
 
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const DATE = moment()
-    .subtract(0, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+  // Use today's date for all commits
+  const DATE = moment().format();
 
   const data = {
     date: DATE,
   };
-  console.log(DATE);
+  console.log(`Committing with date: ${DATE}`);
 
   jsonfile.writeFile(FILE_PATH, data, () => {
     git
       .add([FILE_PATH])
-      .commit(DATE, { "--date": DATE })
-      .push(["-u", "origin", "origin"], (err, result) => {
-        if (err) {
-          console.error("Error pushing to remote:", err);
-        } else {
-          console.log("Pushed changes to remote repository");
-          makeCommit(--n);
-        }
+      .commit(`Commit on ${DATE}`, { "--date": DATE })
+      .then(() => {
+        console.log(`Committed: ${DATE}`);
+        makeCommit(--n);
+      })
+      .catch((err) => {
+        console.error("Error during commit:", err);
       });
   });
 };
 
-makeCommit(120);
+// Call the function to make 20 commits
+makeCommit(20);
+
